a3e1c238c0b11d311711f45469f33addb8b7562b
Makoviczki András <makoviczki.andras@gmail.com>
2015-11-24 18:19:31 +0100

    #Cleaning
    * JobServer cleaned

--- src/main/java/edu/elte/spring/loris/backend/util/JobServer.java
+++ src/main/java/edu/elte/spring/loris/backend/util/JobServer.java
@@ -1,25 +1,32 @@
 package edu.elte.spring.loris.backend.util;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.FileEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,168 +36,173 @@ import org.springframework.scheduling.annotation.Scheduled;
 import edu.elte.spring.loris.backend.entity.FeedEntry;
 import edu.elte.spring.loris.backend.entity.Topic;
 import edu.elte.spring.loris.backend.service.FeedEntryService;
-import edu.elte.spring.loris.backend.service.FeedEntryServiceImpl;
 
 public class JobServer {
 	private static final Logger logger = LoggerFactory.getLogger(JobServer.class);
 
 	CloseableHttpClient client;
 	HttpResponse response;
+
 	@Autowired
 	FeedEntryService feService;
 
 	JobServer() {
 	}
 
-	public void ListContextSB() {
+	public List<String> ListContext(String scheme, String host, Integer port)
+			throws URISyntaxException, JSONException, ParseException, IOException {
+		client = HttpClients.createDefault();
 
-		try {
-			URL url = new URL("http://localhost:8090/contexts");
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod("GET");
+		URI uri = new URIBuilder().setScheme(scheme).setHost(host).setPort(port).setPath("/contexts").build();
+		HttpGet request = new HttpGet(uri);
 
-			StringBuilder result = new StringBuilder();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			String line;
-			while ((line = rd.readLine()) != null) {
-				result.append(line);
+		response = client.execute(request);
+		logger.info("ListContext status:" + response.getStatusLine().toString());
+
+		JSONArray contexts = new JSONArray(EntityUtils.toString(response.getEntity()));
+		List<String> context = new ArrayList<>();
+
+		for (Object object : contexts) {
+			if (object instanceof String) {
+				context.add((String) object);
 			}
-			rd.close();
-
-			logger.info("StringBuilder: " + result.toString());
-
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
+		return context;
 	}
 
-	public void ListContext() {
-		// context get
+	public void CreateContext(String scheme, String host, Integer port, String context, String jarPath)
+			throws URISyntaxException, ClientProtocolException, IOException {
 		client = HttpClients.createDefault();
-		HttpGet request = new HttpGet("http://localhost:8090/contexts");
-		JSONArray contexts;
-		try {
-			response = client.execute(request);
-			contexts = new JSONArray(EntityUtils.toString(response.getEntity()));
 
-			logger.info("JSONArray");
-			for (Object c : contexts) {
-				logger.info(c.toString());
+		URI uri = new URIBuilder().setScheme(scheme).setHost(host).setPort(port).setPath("/contexts/" + context)
+				.addParameter("dependent-jar-uris", jarPath).build();
+		HttpPost post = new HttpPost(uri);
+		response = client.execute(post);
+		logger.info("CreateContext status:" + response.getStatusLine().toString());
+	}
+
+	public void JarUpload(String scheme, String host, Integer port, String programPath, String appName)
+			throws ClientProtocolException, IOException, URISyntaxException {
+		client = HttpClients.createDefault();
+		String uploadFilePath = this.getClass().getResource(programPath).getPath();
+		File uploadFile = new File(uploadFilePath);
+
+		URI uri = new URIBuilder().setScheme(scheme).setHost(host).setPort(port).setPath("/jars/" + appName).build();
+		HttpPost post = new HttpPost(uri);
+
+		FileEntity fileEntity = new FileEntity(uploadFile, ContentType.APPLICATION_OCTET_STREAM);
+		fileEntity.setChunked(true);
+		post.setEntity(fileEntity);
+
+		response = client.execute(post);
+		logger.info("JarUpload status:" + response.getStatusLine().toString());
+	}
+
+	public String StartJob(String scheme, String host, Integer port, Map<String, String> paramters,
+			Map<String, String> postParameters) throws URISyntaxException, ClientProtocolException, IOException {
+
+		client = HttpClients.createDefault();
+
+		URIBuilder uriBuilder = new URIBuilder().setScheme(scheme).setHost(host).setPort(port).setPath("/jobs");
+
+		for (Entry<String, String> e : paramters.entrySet()) {
+			uriBuilder.addParameter(e.getKey(), e.getValue());
+		}
+
+		URI uri = uriBuilder.build();
+		HttpPost post = new HttpPost(uri);
+		ArrayList<NameValuePair> postParameterList = new ArrayList<NameValuePair>();
+
+		for (Entry<String, String> e : postParameters.entrySet()) {
+			postParameterList.add(new BasicNameValuePair(e.getKey(), e.getValue()));
+		}
+
+		post.setEntity(new UrlEncodedFormEntity(postParameterList));
+
+		response = client.execute(post);
+		logger.info("StartJob status:" + response.getStatusLine().toString());
+		
+		if(response.getStatusLine().getStatusCode() != 200){
+			logger.warn("JobServer can't finished the process!");
+			return null;
+		}
+
+		return EntityUtils.toString(response.getEntity());
+	}
+
+	public void ProcessJSON(String json) {
+		JSONObject fullResult = new JSONObject(
+				StringEscapeUtils.unescapeJava(json).replace("\"[", "[").replace("]\"", "]"));
+
+		JSONArray resultBody = fullResult.getJSONArray("result");
+
+		List<Topic> topics = new ArrayList<>();
+
+		for (int i = 0; i < resultBody.length(); ++i) {
+			JSONObject rowIdTopicPair = resultBody.getJSONObject(i);
+			String rowId = rowIdTopicPair.getString("rowId");
+			JSONArray topicArray = rowIdTopicPair.getJSONArray("topic");
+
+			FeedEntry fe = feService.findFeedEntry(rowId);
+
+			for (int j = 0; j < topicArray.length(); ++j) {
+				Topic topic = new Topic();
+				JSONObject topicPair = topicArray.getJSONObject(j);
+
+				topic.setFeedEntry(rowId);
+				topic.setTopicName(topicPair.getString("topicName"));
+				topic.setTopicValue(topicPair.getDouble("topicValue"));
+
+				topics.add(topic);
 			}
-		} catch (Exception e) { //
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			fe.setLabeled(true);
+			fe.setTopic(topics);
+
+			feService.updateFeedEntry(fe);
 		}
+		logger.info("JSON processing successful");
 	}
 
-	public void CreateContext() {
-		client = HttpClients.createDefault();
-		String contextName = "myContext";
-
-		HttpPost post = new HttpPost("http://localhost:8090/contexts/" + contextName + "?dependent-jar-uris=file:///home/andris/jars/bundle.jar");
-
-		try {
-			response = client.execute(post);
-			logger.info(response.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void JarUpload() {
-		// jar upload - multipart
-		client = HttpClients.createDefault();
-		try {
-			String uploadFilePath = this.getClass().getResource("/resources/ScalaPi-0.0.1-SNAPSHOT.jar").getPath();
-			//String uploadFilePath = this.getClass().getResource("/resources/pi-0.0.1-SNAPSHOT.jar").getPath();
-			String appName = "newjar";
-
-			File uploadFile = new File(uploadFilePath);
-			HttpPost post = new HttpPost("http://localhost:8090/jars/" + appName);
-
-			// MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-			// //builder.addBinaryBod
-			// builder.addBinaryBody("new", p,
-			// ContentType.APPLICATION_OCTET_STREAM,"new.jar");
-			// HttpEntity multipart = builder.build();
-			// post.setEntity(multipart);
-
-			FileEntity fileEntity = new FileEntity(uploadFile, ContentType.APPLICATION_OCTET_STREAM);
-			fileEntity.setChunked(true);
-			post.setEntity(fileEntity);
-
-			response = client.execute(post);
-			logger.info("file upload:" + response.getStatusLine().toString());
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	public void StartJob() {
-		// start new job
-		client = HttpClients.createDefault();
-		try {
-			HttpPost post = new HttpPost(
-					new URL("http://localhost:8090/jobs?appName=newjar&classPath=edu.elte.spring.loris.spark.model.Lda&context=myContext&sync=true&timeout=300")
-							.toURI());
-			
-			ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
-			/*postParameters.add(new BasicNameValuePair("pi.arg", "10"));
-			post.setEntity(new UrlEncodedFormEntity(postParameters));*/
-			
-			response = client.execute(post);
-
-			//eredmény
-			String s = EntityUtils.toString(response.getEntity());
-			logger.info(response.getStatusLine().toString());
-			logger.info(s);
-			
-			JSONObject fullResult = new JSONObject(StringEscapeUtils.unescapeJava(s).replace("\"[", "[").replace("]\"", "]"));
-			JSONArray resultBody = fullResult.getJSONArray("result");
-			
-			List<Topic> topics = new ArrayList<>();
-			
-			for(int i = 0; i < resultBody.length();++i){
-				JSONObject rowIdTopicPair = resultBody.getJSONObject(i);
-				String rowId = rowIdTopicPair.getString("rowId");
-				JSONArray topicArray = rowIdTopicPair.getJSONArray("topic");// getJSONArray("topic");
-				
-				FeedEntry fe = new FeedEntry();
-				fe = feService.findFeedEntry(rowId);
-				
-				for(int j = 0; j < topicArray.length();++j){
-					Topic topic = new Topic();
-					JSONObject topicPair = topicArray.getJSONObject(j);
-					
-					topic.setFeedEntry(rowId);
-					topic.setTopicName(topicPair.getString("topicName"));
-					topic.setTopicValue(topicPair.getDouble("topicValue"));
-					
-					topics.add(topic);
-				}
-				
-				fe.setLabeled(true);
-				fe.setTopic(topics);
-				
-				feService.updateFeedEntry(fe);
-				logger.info("Updated" + fe.toString());
-			}
-			
-		} catch (Exception e) { //
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	// @Async
-	@Scheduled(fixedDelay = 60000)
+	@Scheduled(fixedDelay = 300000)
 	public void JobServerServiceMethod() {
-		CreateContext();
-		ListContext();
-		JarUpload();
-		StartJob();
-		//logger.info("asd");
+
+		String scheme = "http";
+		String host = "localhost";
+		Integer port = 8090;
+		String contextName = "lorisContext";
+		String jarPath = "file:///home/andris/jars/bundle.jar";
+		String appPath = "/resources/ScalaPi-0.0.1-SNAPSHOT.jar";
+		String appName = "lorisSpark";
+		String classPath = "edu.elte.spring.loris.spark.model.Lda";
+		String sync = "true";
+		String timeOut = "10000";
+
+		Map<String, String> appParameters = new HashMap<>();
+		appParameters.put("appName", appName);
+		appParameters.put("classPath", classPath);
+		appParameters.put("context", contextName);
+		appParameters.put("sync", sync);
+		appParameters.put("timeout", timeOut);
+
+		Map<String, String> appArguments = new HashMap<>();
+
+		try {
+			List<String> contexts = ListContext(scheme, host, port);
+
+			if (!contexts.contains(contextName)) {
+				CreateContext(scheme, host, port, contextName, jarPath);
+			}
+
+			JarUpload(scheme, host, port, appPath, appName);
+			String result = StartJob(scheme, host, port, appParameters, appArguments);
+
+			if (result != null) {
+				ProcessJSON(result);
+			}
+		} catch (JSONException | ParseException | URISyntaxException | IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
--- src/main/java/edu/elte/spring/loris/backend/util/StreamingChannel.java
+++ src/main/java/edu/elte/spring/loris/backend/util/StreamingChannel.java
@@ -1,31 +1,11 @@
 package edu.elte.spring.loris.backend.util;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.Date;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import com.sun.syndication.io.FeedException;
-
-import edu.elte.spring.loris.backend.entity.Category;
-import edu.elte.spring.loris.backend.entity.Channel;
-import edu.elte.spring.loris.backend.entity.FeedEntry;
-import edu.elte.spring.loris.backend.entity.Subscription;
-import edu.elte.spring.loris.backend.service.CategoryService;
-import edu.elte.spring.loris.backend.service.CategoryServiceImpl;
-import edu.elte.spring.loris.backend.service.ChannelService;
-import edu.elte.spring.loris.backend.service.ChannelServiceImpl;
-import edu.elte.spring.loris.backend.service.FeedEntryService;
-import edu.elte.spring.loris.backend.service.FeedEntryServiceImpl;
 import edu.elte.spring.loris.backend.service.SubscriptionService;
 
 public class StreamingChannel {
-	private static final Logger logger = LoggerFactory.getLogger(StreamingChannel.class);
 
 	@Autowired
 	SubscriptionService sService;
@@ -33,8 +13,7 @@ public class StreamingChannel {
 	public StreamingChannel() {
 	}
 
-	//@Async
-	@Scheduled(fixedDelay = 60000)
+	@Scheduled(fixedDelay = 300000)
 	public void Streaming(){
 		sService.Download();
 	}
